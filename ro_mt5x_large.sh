91fa98da4dabf50e16256f5a436e915cfe9916d3
dumitrescustefan <dumitrescu.stefan@gmail.com>
2022-06-09 15:45:18 +0200

    up

--- ro_mt5x_large.sh
+++ ro_mt5x_large.sh
@@ -1,10 +1,11 @@
 PROJECT_DIR=${HOME}"/models/t5x_models"
 T5X_DIR="../../t5x"  # directory where the t5x is cloned.
-MODEL_DIR="gs://t5x-training/pretrained_models/test"
 MODEL_DIR="gs://myv4-bucket/ro_mt5x_large"
+GIN_FILE="ro_mt5x_large.gin"
+
 export PYTHONPATH=${PROJECT_DIR}
 
 python3 ${T5X_DIR}/t5x/train.py \
   --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="ro_mt5_large.gin" \
+  --gin_file=${GIN_FILE} \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
