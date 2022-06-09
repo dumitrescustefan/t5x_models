ba0823133c842743102a420b3d349d79dbb68387
dumitrescustefan <dumitrescu.stefan@gmail.com>
2022-06-09 12:10:26 +0200

    up

--- train_large_test.sh
+++ train_large_test.sh
@@ -1,10 +1,10 @@
 PROJECT_DIR=${HOME}"/models/pk-nb-t5x"
 T5X_DIR="../../t5x"  # directory where the t5x is cloned.
 MODEL_DIR="gs://t5x-training/pretrained_models/test"
-MODEL_DIR="gs://myv4-bucket/t5x_large2"
+MODEL_DIR="gs://myv4-bucket/mt5x_large"
 export PYTHONPATH=${PROJECT_DIR}
 
 python3 ${T5X_DIR}/t5x/train.py \
   --gin_search_paths=${PROJECT_DIR} \
-  --gin_file="norwegian_large.gin" \
+  --gin_file="ro_mt5_large.gin" \
   --gin.MODEL_DIR="'${MODEL_DIR}'" \
